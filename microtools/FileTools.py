fea547421672a6010ccad51321219d421513d801
ayakushko <ayakushko@intropro.com>
2016-09-04 13:24:25 +0300

    [+] added patterned file search to fileTools

--- microtools/FileTools.py
+++ microtools/FileTools.py
@@ -1,13 +1,45 @@
+import os
+import fnmatch
 def putContents(name, data):
+    """
+    saves content to file
+    :param name:
+    :param data:
+    :return:
+    """
     f=open(name, "w+")
     f.write(data)
     f.close()
 
 
 def getContents(name):
+    """
+    returns file data
+    :param name:
+    :return:
+    """
     f = open(name, "r")
     try:
         return f.read()
     finally:
         f.close()
+
+def getDirContentsRec(dir, pattern=None):
+    """
+    returns dir content, including subdirectories
+    :param dir:
+    :type dir: str
+    :return: returns list of files
+    :rtype: list[str]
+    """
+    result=[]
+    files = os.listdir(dir)
+    for file in files:
+        fullPath=os.path.join(dir, file)
+        if os.path.isdir(fullPath):
+            result += getDirContentsRec(fullPath, pattern)
+        else:
+            if not pattern or fnmatch.fnmatch(fullPath, pattern):
+                result.append(fullPath)
+    return result
 
--- tests/FileToolsTest.py
+++ tests/FileToolsTest.py
@@ -14,11 +14,17 @@ class FileToolsTest(unittest.TestCase):
         readed = FileTools.getContents(TEST_FILE)
         self.assertEqual(readed, TEST_DATA)
 
+    def test_getDirContentsRec(self):
+        res = FileTools.getDirContentsRec("./")
+        self.assertTrue("./testFiles/test1" in res)
+        self.assertTrue("./testFiles/test2" in res)
+        res = FileTools.getDirContentsRec("./", "*2")
+        self.assertFalse("./testFiles/test1" in res)
+        self.assertTrue("./testFiles/test2" in res)
+
     def tearDown(self):
-        os.remove(TEST_FILE)
+        if os.path.exists(TEST_FILE):
+            os.remove(TEST_FILE)
 
 if __name__ == "__main__":
     unittest.main()
-
-
-
